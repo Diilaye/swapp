c63eccd212a62d0be373d75d6f25a259eff6553f
Issa laye KANE <issa.k@deally.fr>
2023-11-27 16:17:15 +0000

    add news term of policy

--- app.js
+++ app.js
@@ -7,8 +7,11 @@ const app = express();
 const port = 5400;
 
 app.use(express.static(__dirname + '/web/'));
+app.use(express.static(__dirname + '/theme/'));
 
 app.get('/', (req, res) => res.sendFile(path.resolve('web/index.html')));
+app.get('/terms-of-service', (req, res) => res.sendFile(path.resolve('theme/index.html')));
+app.get('/*', (req, res) => res.sendFile(path.resolve('web/index.html')));
 
 const server = http.createServer(app);
 
